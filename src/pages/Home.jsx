a05ae2449297f23faf9da13af1930ba88db1ccae
Ken Robert Ochieng <kenrobertochi@gmail.com>
2025-04-19 23:56:19 +0300

    added images in db.json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,28 @@
 import React from "react"
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import Footer from "../components/Footer"
 function Home({recipes}){
-
+    const [loading,setloading]=useState('')
     const navigate=useNavigate()
     const mappedRecipes=recipes.map((recipe)=>{
         const handleViewClick=()=>{
-            navigate(`/recipes/${recipe.id}`,{state:recipe})
+            setloading(recipe.id)
+            setTimeout(()=>{
+                navigate(`/recipes/${recipe.id}`,{state:recipe})
+            },4000)
         }
         return(
             <div key={recipe.id} className="main-section">
                 <h1>{recipe.name}</h1>
                 <img src={recipe.image} alt={recipe.name}/>
                 <p><span>Description: </span>{recipe.description}</p>
+
+               {loading===recipe.id
+                &&
+                <p className="loading-data">Just a sec....🤏</p>
+                }
+    
                 <div className="myBtn">
                 <button onClick={handleViewClick}>View Recipe</button>
                 <button>Edit Recipe</button>
@@ -21,9 +32,12 @@ function Home({recipes}){
         )
     })
     return(
+        <>
         <div className="my-home">
         {mappedRecipes}
+        <Footer/>
             </div>
-    )
+            </>    
+            )
 }
 export default Home
