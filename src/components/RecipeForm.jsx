c22a8ac3702d98039c82f4cca00386b63b6527e4
Ken Robert Ochieng <kenrobertochi@gmail.com>
2025-04-20 22:41:58 +0300

    made changes

---
+++ src/components/RecipeForm.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react"
+
+
+function RecipeForm({taskAdd}){
+    const [name,setName]=useState('')
+    const [description,setDescription]=useState('')
+    const [ingredients,setIngredients]=useState('')
+    const [instructions,setInstructions]=useState('')
+    const [imageUrl, setImageUrl] = useState("");
+    
+    //function for adding recipe
+    function handleSubmit(e){
+        e.preventDefault()
+        const addedRecipe={
+            name:name,
+            description:description,
+            ingredients:ingredients,
+            instructions:instructions,
+            image:imageUrl
+        }       
+         fetch('http://localhost:3000/recipes',{
+            method:'POST',
+            headers:{
+                'Content-Type':'appliction/json'
+            },
+            body:JSON.stringify(addedRecipe)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            setName("")
+            setDescription("")
+            setIngredients("")
+            setInstructions("")
+            setImageUrl("");
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    }
+    return(
+        <div className="my-add">
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Recipe name..." value={name} onChange={e=> setName(e.target.value)}/>
+                <input type="text" placeholder="Recipe description..."value={description} onChange={e=>setDescription(e.target.value)} />
+                <input type="text" placeholder="Recipe ingredients..." value={ingredients} onChange={e=>setIngredients(e.target.value)} />
+                <input type="text" placeholder="Recipe instructions..." value={instructions} onChange={e=>(setInstructions(e.target.value))}/>
+                <input type="text" placeholder="Image URL..." value={imageUrl} onChange={e => setImageUrl(e.target.value)}/>
+                <button type="submit">Add Recipe</button>
+            </form>
+        </div>
+    )
+}
+export default RecipeForm
+
