b081e3d417d3c4f2cef08ade62d2c053b6abebeb
Andrew <andrewmandieka@gmail.com>
2025-04-23 18:45:01 +0300

    Updated by Andrew

--- src/pages/RecipeDetais.jsx
+++ src/pages/RecipeDetais.jsx
@@ -1,18 +1,31 @@
-import { useLocation } from "react-router-dom"
-function RecipeDetails({setRecipes}){
-    const location=useLocation()
-    const data=location.state
-    console.log(location.state)
+import { useLocation } from "react-router-dom";
+import "./RecipeDetails.css"; 
 
-    return(
-        <div className="main-section">
-               <h1>{data.name}</h1>
-                <img src={data.image} alt={data.name} ></img>
-                <p><span>Description: </span>{data.description}</p>
-                <p><span>Ingredients: </span>{data.ingredients}</p>
-                <p> <span>Instructions: </span>{data.instructions}</p>
+function RecipeDetails({ setRecipes }) {
+  const location = useLocation();
+  const data = location.state;
+  console.log(location.state);
+
+  return (
+    <div className="recipe-details-container">
+      <div className="recipe-header">
+        <h1>{data.name}</h1>
+        <img src={data.image} alt={data.name} className="recipe-image" />
+      </div>
+
+      <div className="recipe-info">
+        <div className="details-section">
+          <p><span className="info-label">Description:</span> {data.description}</p>
         </div>
-    )
+        <div className="details-section">
+          <p><span className="info-label">Ingredients:</span> {data.ingredients}</p>
+        </div>
+        <div className="details-section">
+          <p><span className="info-label">Instructions:</span> {data.instructions}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default RecipeDetails
 
+export default RecipeDetails;
